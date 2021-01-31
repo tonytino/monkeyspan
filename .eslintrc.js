3876a82eb716cd1a71552ce469b611099e5da4bc
Anthony Hernandez <anthony.hernandez@affirm.com>
2021-01-30 23:01:35 -0800

    Add and configure Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  plugins: ['react'],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  plugins: ["react"],
   rules: {},
 };

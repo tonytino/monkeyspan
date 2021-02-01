b6ee0663f7ff0c596370d758226e073abc1c231f
Anthony Hernandez <anthony.hernandez@affirm.com>
2021-02-01 02:39:37 -0800

    Add eslint plugins for testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,11 @@ module.exports = {
     "airbnb",
     "prettier",
     "plugin:jest/all",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/recommended",
+    "plugin:testing-library/react",
   ],
-  plugins: ["jest", "react"],
+  plugins: ["jest", "react", "jest-dom", "testing-library"],
   rules: {
     "react/jsx-props-no-spreading": "off",
     "jest/prefer-expect-assertions": "off",
